a1ebeb3d8366bfb58f0612d39ba86528e4bb8d4d
rennelou <rennelou@outlook.com>
2020-09-09 20:58:22 -0300

    primeiro teste do automato

--- *(aaa)*/automaton.vhd
+++ *(aaa)*/automaton.vhd
@@ -1,36 +1,41 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
-use ieee.numeric_std.all;
 
 entity automaton is 
     port(
+        clock: in std_logic;
         s_reset: in std_logic;
-        input: in std_logic_vector(0 to 255);
+        input: in character;
         s_accepted: out std_logic
     );
 end automaton;
 
 architecture recognize of automaton is
     type STATES is (q0, a0, a1, a2);
-    signal state: STATES := q0;
+    signal actual_state, next_state: STATES := q0;
 
 begin
-    transition: process(input)
-        if input = 'a' then
-            case state is
-                when q0 => state <= a0;
-                when a0 => state <= a1;
-                when a1 => state <= a2;
+    tick: process(clock)
+    begin
+        if falling_edge(clock) then
+            actual_state <= next_state;
+        end if;
+    end process tick;
+
+    transition: process(actual_state, input, s_reset)
+    begin
+        if (input = 'a') then
+            case actual_state is
+                when q0 => next_state <= a0;
+                when a0 => next_state <= a1;
+                when a1 => next_state <= a2;
+                when a2 => next_state <= a2;
             end case;
+        elsif falling_edge(s_reset) then
+            next_state <= q0;
         end if;
     end process transition;
     
-    s_accepted <= state = a2;
-    
-    reset: process(r_reset)
-        if falling_edge(s_reset) then
-            state <= q0;
-        end if;
-    end process reset;
+    s_accepted <= '1' when actual_state = a2 else '0';
 
 end recognize;
---
+++ *(aaa)*/unitTest.vhd
@@ -0,0 +1,58 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use std.textio.all;
+
+entity unitTest is
+    end unitTest;
+    
+    architecture test of unitTest is
+        
+        constant period: time := 10ns;    
+        constant number_cycle: integer := 20;
+
+        component clock
+            port(
+                period: in time;
+                number_cycle: in integer;
+                output: out std_logic
+            );
+        end component;
+
+        component automaton
+            port(
+                clock: in std_logic;
+                s_reset: in std_logic;
+                input: in character;
+                s_accepted: out std_logic
+            );
+        end component;
+
+        signal w_clock: std_logic := '0';
+        signal s_reset: std_logic := '0';
+        signal s_input: character;
+        signal s_accepted: std_logic := '0';
+
+    begin
+        CLOCK1: clock port map(period, number_cycle, w_clock);
+        A0: automaton port map(w_clock, s_reset, s_input, s_accepted);
+
+        test: process
+        begin    
+            s_input <= 'b';
+            wait for period;
+            s_input <= 'a';
+            wait for period;
+            s_input <= 'a';
+            wait for period;
+            s_input <= 'a';
+            wait for period;
+            s_input <= 'b';
+            wait for period;
+            
+            s_reset <= '1';
+            wait for 2*period;
+            s_reset <= '0';
+
+            wait;
+        end process test;
+    end test;
