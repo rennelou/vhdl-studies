fdb0e850a5cadc7166a2e0458ef463382c70d8f4
rennelou <rennelou@outlook.com>
2020-09-20 14:28:27 -0300

    next_state <= actual_state;

--- *(aaa)*/automaton.vhd
+++ *(aaa)*/automaton.vhd
@@ -18,26 +18,25 @@ architecture recognize of automaton is
     signal actual_state, next_state: STATES := q0;
 
 begin
-    tick: process(clock)
+    tick: process(clock, s_reset)
     begin
-        if falling_edge(clock) then
+        if falling_edge(s_reset) then
+            actual_state <= q0;
+        elsif rising_edge(clock) then
             actual_state <= next_state;
         end if;
     end process tick;
 
-    transition: process(actual_state, input, s_reset)
+    transition: process(actual_state, input)
     begin
-        if falling_edge(s_reset) then
-            next_state <= q0;
-        else
-            if (input = 'a') then
-                case actual_state is
-                    when q0 => next_state <= a0;
-                    when a0 => next_state <= a1;
-                    when a1 => next_state <= a2;
-                    when a2 => next_state <= a2;
-                end case;
-            end if;
+        next_state <= actual_state;
+        if (input = 'a') then
+            case actual_state is
+                when q0 => next_state <= a0;
+                when a0 => next_state <= a1;
+                when a1 => next_state <= a2;
+                when a2 => next_state <= a2;
+            end case;
         end if;
     end process transition;
     
